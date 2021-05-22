d264576fdb658fc4a9cc07efed2a6e03fa1b27a0
Hugo Ferreira <46359390+hugofer19@users.noreply.github.com>
2021-05-22 17:52:15 +0100

    Add files via upload

---
+++ db.py
@@ -0,0 +1,6 @@
+class Database:
+    def __init__(self):
+        self.Host = "ec2-54-155-35-88.eu-west-1.compute.amazonaws.com"
+        self.Database = "d6ttuoeki1a7o9"
+        self.User = "amefnawljvnlms"
+        self.Password = "bdc35b7aad8f479f0b75fb40a1f719d4fb97cdd626ecb13538a1ae848bb50bd8"
